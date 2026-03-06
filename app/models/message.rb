0045132ae19a63097c37c3165062fd671b48165a
Amandyne Desfonds <amandyne.desfonds@gmail.com>
2026-03-06 12:29:15 +0100

    Disable broken broadcast and autoscroll

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,25 +5,4 @@ class Message < ApplicationRecord
 
   validates :role, inclusion: { in: %w[system user assistant] }
   validates :content, presence: true
-
-  after_create_commit :broadcast_message, unless: :system?
-
-  private
-
-  def system?
-    role == "system"
-  end
-
-  def broadcast_message
-    html = ApplicationController.render(
-      partial: "messages/message",
-      locals: { message: self }
-    )
-
-    Turbo::StreamsChannel.broadcast_append_to(
-      "chat_#{chat.id}_messages",
-      target: "messages",
-      html: html
-    )
-  end
 end
